cb026897887839a8c972e5c303777add48c839bf
Євгеній Москаленко <moskale2yevhenii@hneu.net>
2019-12-04 10:44:37 +0300

    install browser sync

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass");
+const browserSync = require("browser-sync");
 
 // gulp.task("mytask", () => {
 //   return gulp
@@ -7,9 +8,19 @@ const sass = require("gulp-sass");
 //     .pipe(plagin())
 //     .pipe(gulp.dest("folder"));
 // });
-gulp.task("sass", () => {
+gulp.task("sass", function() {
   return gulp
     .src(["!app/sass/main.sass", "app/sass/**/*.sass"])
     .pipe(sass())
     .pipe(gulp.dest("app/css"));
+});
+gulp.task("watches", function() {
+  gulp.watch("app/sass/**/*.sass", gulp.parallel("sass"));
+});
+gulp.task("Brawser", () => {
+  browserSync({
+    server: {
+      baseDir: "app"
+    }
+  });
 });
